b47396b88ccbe1515f036a484271880ae3c2a16b
Liliia Baieva <555blv@gmail.com>
2023-03-09 11:33:32 +0100

    add constructor with cheсk exceptions

--- src/Triangle.java
+++ src/Triangle.java
@@ -5,7 +5,21 @@ public class Triangle {
   private int sideC;
 
 
+  // В конструкторе нужно проверить введённые значения на корректность
+  // (например, длины не могут быть отрицательными).
+  public Triangle(int sideA, int sideB, int sideC){
+    try{
+      if(sideA <= 0 || sideB <= 0 || sideC <= 0){
+        System.out.println("The length of side can not be negative.");
+      }
+      this.sideA = sideA;
+      this.sideB = sideB;
+      this.sideC = sideC;
+    } catch (NumberFormatException e){
+      System.out.println("Incorrect number entry: " + e.getMessage());
+    }
 
+  }
 
 
 
