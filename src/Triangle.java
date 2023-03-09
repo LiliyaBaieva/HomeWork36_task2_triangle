9f2320e0314558b08499906436b8eefb87ba295a
Liliia Baieva <555blv@gmail.com>
2023-03-09 11:20:35 +0100

    add class Triangle

---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+  // Создайте класс Triangle (треугольник), принимающий три целых значения - длины трёх сторон треугольника.
+  private int sideA;
+  private int sideB;
+  private int sideC;
+
+
+
+
+
+
+}
